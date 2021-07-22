dd65fafdc57ae59126850e93b96c565760d041ff
AlayKharadi <62132315+AlayKharadi@users.noreply.github.com>
2021-07-22 13:38:53 +0530

    Instructions Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,36 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Jumbotron } from "react-bootstrap";
+import { useState } from "react";
+import { Tabs, Tab } from "react-bootstrap";
+import Offline from "./Offline";
+import Online from "./Online";
 
 function Home() {
+	const [key, setKey] = useState('online');
 
 	return (
-		<Jumbotron style={{ marginBottom: '0px' }}>
-			<h1>Hello from ROSDS!</h1>
-			<hr />
-			<h2>Go to <a href="https://app.theconstructsim.com/" target="_blank" rel="noreferrer">Constructsim</a></h2>
+		<div className="container" style={{ marginBottom: '0px' }}>
+			<h3>Introduction:</h3>
 			<ul>
-				<li>
-					<p>create account</p>
-				</li>
-				<li>
-					<p>Go to Project</p>
-				</li>
-				<li>
-					<p>create  a new Project</p>
-				</li>
-				<li>
-					<p>Run the Project</p>
-				</li>
-				<li>
-					<p>Run ROSBridge Websocket Server via terminal</p>
-					<p>command:<code> roslaunch rosbridge_server rosbridge_websocket.launch</code></p>
-				</li>
+				<li>We have created an online website through which you can control the ROS robot.</li>
+				<li>Below you have instructions for how to use the website both in online or offline mode.</li>
+				<li>you can find controller in control section.</li>
+				<li>logs regarding the connection process are in logs section.</li>
+				<li>This is part of our Institute's project. If you want to contribute to this project this is the link <a href="https://github.com/AlayKharadi/Ros-Web-App" target="_blank" rel="noreferrer">Github Project</a>. feel free to add something new to this project.</li>
+
 			</ul>
-		</Jumbotron>
+			<Tabs
+				id="controlled-tab-example"
+				activeKey={key}
+				onSelect={(k) => setKey(k)}
+				className="mb-3"
+			>
+				<Tab eventKey="online" title="Online">
+					<Online></Online>
+				</Tab>
+				<Tab eventKey="offline" title="Offline">
+					<Offline></Offline>
+				</Tab>
+			</Tabs>
+			</div>
 	);
 }
 
---
+++ src/components/Offline.js
@@ -0,0 +1,40 @@
+const Offline = () => {
+    return (
+        <div>
+            <h3>To connect in Linux ROS Enviornment</h3>
+            <hr />
+            <ul>
+                <li>
+                    <p>Open terminal run below command.</p>
+                    <p>command : <code>roscore</code></p>
+                </li>
+                <li>
+                    <p>Open another terminal and run below command.</p>
+                    <p>command : <code>rosrun turtlesim turtlesim_node</code></p>
+                </li>
+                <li>
+                    <p>Run ROSBridge Websocket Server via another terminal</p>
+                    <p>command : <code>roslaunch rosbridge_server rosbridge_websocket.launch</code></p>
+                </li>
+                <li>
+                    <p>Write <code>ws://localhost:9090</code> or <code>ws://127.0.0.1:9090</code> it in our website's control section's websocket server address field.</p>
+                </li>
+                <li>
+                    <p>controller will be enabled as soon as you click <code>connect</code> button.</p>
+                </li>
+                <li>
+                    <p>Put <code>/turtle1/cmd_vel</code> in the Topic Name to use controller.</p>
+                </li>
+                <li>
+                    <p>You are good to go.</p>
+                </li>
+                <li>
+                    <p>You can see your robot moving the constructsim's simulator.</p>
+                </li>
+                
+            </ul>
+        </div>
+    );
+}
+
+export default Offline;
